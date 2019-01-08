6031d50f4d252f16a43b9f4eb1e03cfcbc5d40b7
Moritz Heusinger <moritz.heusinger@gmail.com>
2019-01-08 12:16:44 +0100

    bump gulp

--- gulpfile.js
+++ gulpfile.js
@@ -482,4 +482,4 @@ gulp.task('updateReadme', function (done) {
     done();
 });
 
-gulp.task('default', ['updatePackages', 'updateReadme']);
+gulp.task('default', gulp.series('updatePackages', 'updateReadme'));
